64069979443a590aae55917a0651625598f905dd
jin cai <genecai@gmail.com>
2020-03-05 14:03:17 -0700

    dropdown for sex, when error set to default values

--- run.py
+++ run.py
@@ -6,37 +6,37 @@ from src.discount_curve_module import DiscountCurveProvider
 app = Flask(__name__)
 
 def get_params(form_data):
-    #if form_data.get("age") is None:
-        
-    #else:
-    age = int(form_data.get("age"))
-    
-    #if form_data.get("age") is None:
-        
-    #else:
-    sex = str(form_data.get("sex"))
-    
-    #if form_data.get("age") is None:
-        
-    #else:
-    payment = float(form_data.get("payment"))
-    
-    
+    try:
+        age = int(form_data.get("age"))
+    except:
+        age = 65
+    try:
+        sex = str(form_data.get("sex"))
+    except:
+        sex = 'm'
+    try:
+        payment = float(form_data.get("payment"))
+    except:
+        payment = 2000
     return age, sex, payment
 
 @app.route("/", methods=["GET", "POST"])
 def annunity_price():
     if request.method == "POST":
         age, sex, payment = get_params(request.form)
-
         swap = pd.read_csv('./src/swaps.csv')
         dcp = DiscountCurveProvider(tenors = swap['Tenor'], parcurve = swap['Swap'])
+            
+        try:
+            price = dcp.bootstrap()[0]
+        except:
+            price = 0
 
         return render_template("app.html",
-                               age=age,
-                               sex=sex,
-                               payment=payment,
-                               result = dcp.bootstrap()[0]
+                               age = age,
+                               sex = sex,
+                               payment = payment,
+                               result = price
                                )
     else:
         return render_template("app.html")
