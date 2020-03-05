f6b9446bff14f2b908f2063fddaa6f1af6bc5249
jin cai <genecai@gmail.com>
2020-03-05 10:08:39 -0700

    read swap from csv

--- src/swap_rate_module.py
+++ src/swap_rate_module.py
@@ -1,10 +1,15 @@
+import pandas as pd
+
 class SwapRateProvider():
 
     def get_swap_rates(self):
         print('not implemented yet')
 
+    def get_swap_from_csv(self):
+        return pd.read_csv('./src/swaps.csv')
+
 
 if __name__ == "__main__":
     srp = SwapRateProvider()
 
-    print('Swap Rates')
+    print(srp.get_swap_from_csv())
