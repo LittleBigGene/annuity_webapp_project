6583f54d8ad90101fa0d72294142577604bf0ed8
jin cai <genecai@gmail.com>
2020-03-05 09:54:41 -0700

    read mortality table from csv

--- src/mortality_table_module.py
+++ src/mortality_table_module.py
@@ -1,12 +1,14 @@
+import pandas as pd
+
 class MortalityTableProvider():
     def get_table_from_SOA(self):
         print('not implemented yet')
 
-
-        
-
+    def get_table_from_csv(self):
+        return pd.read_csv('./src/mortality.csv')
 
 if __name__ == "__main__":
     mtp = MortalityTableProvider()
 
-    print('SOA Mortality Tables')
+    print(mtp.get_table_from_csv())
+    
