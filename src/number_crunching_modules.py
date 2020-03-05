30ef9a22407b1925c056882d8b44a53c6c59ec64
jin cai <genecai@gmail.com>
2020-03-05 10:34:47 -0700

    Bring in the info needed. Has run time bug. Ben, would you like to take the lead this ?

--- src/number_crunching_modules.py
+++ src/number_crunching_modules.py
@@ -1,11 +1,15 @@
 import numpy as np
+from mortality_table_module import MortalityTableProvider
+from discount_curve_module import DiscountCurveProvider
+from swap_rate_module import SwapRateProvider
 class NumberCruncher():
 
-    def __init__(self, age, sex, payment, mortality):
+    def __init__(self, age, sex, payment, mortality, discount):
         self.age = age
         self.sex = sex
         self.payment = payment
         self.mortality = mortality
+        self.discount = discount
 
     def update_age(self, age):
         self.age = age
@@ -16,11 +20,12 @@ class NumberCruncher():
     def update_payment(self, payment):
         self.payment = payment
 
-    def actuarial_PV(self, discount_rates):
+
+    def actuarial_PV(self):
         for x in range (0, len(self.mortality)):
             tpx = np.prod((1 - np.array(self.mortality)))
             tpx_array = np.array(tpx[:x] )
-        return np.inner(tpx_array, discount_rates)
+        return np.inner(tpx_array, self.discount)
 
     def life_expectancy(self):
         for x in range (0, len(self.mortality)):
@@ -32,6 +37,15 @@ if __name__ == "__main__":
     age = 50
     sex = 'm'
     payment = 1000
-    crunch = NumberCruncher(age, sex, payment)
 
-    print('results')
+    mtp = MortalityTableProvider()
+    mortality = mtp.get_table_from_csv()
+
+    srp = SwapRateProvider()
+    swap = srp.get_swap_from_csv()
+
+    dcp = DiscountCurveProvider(tenors = swap['Tenor'], parcurve = swap['Swap'])
+
+    crunch = NumberCruncher(age, sex, payment, mortality, dcp.bootstrap())
+
+    print(crunch.actuarial_PV())
